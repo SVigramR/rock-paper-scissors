4b623cbd109f62c6a6386459bf6f8e97c3cb7eb6
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-05-11 23:32:24 +0530

    Added computerPlay function for computer's choice

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log("Rock-Paper-Scissors");
+
+function computerPlay() {
+    let compOutput = Math.floor((Math.random() * 3) + 1);
+    if (compOutput === 1) {
+        return "rock";
+    } else if (compOutput === 2) {
+        return "paper";
+    } else if (compOutput === 3) {
+        return "scissors";
+    } else {
+        console.log("None")
+    }
+}
+
+console.log("Computer's choice:", computerPlay())
