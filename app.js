8d134e1f059fa460213a9659a06552abd01557b6
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-05-12 20:13:42 +0530

    Added playRound function to find the winner
    // Getting input from the user using prompt
    // playRound function has two parameters which are computer's choice and player's choice

--- app.js
+++ app.js
@@ -13,4 +13,32 @@ function computerPlay() {
     }
 }
 
-console.log("Computer's choice:", computerPlay())
+// console.log("Computer's choice:", computerPlay())
+
+let playerChoice = prompt("Type your choice: ").toLowerCase();
+let computerChoice = computerPlay();
+console.log(playRound(playerChoice, computerChoice));
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection==='rock' && computerSelection==='scissors') {
+        return "You Won! Rock beats Scissors"
+    } else if (playerSelection==='paper' && computerSelection==='rock') {
+        return "You Won! Paper beats Rock"
+    } else if (playerSelection==='scissors' && computerSelection==='paper') {
+        return "You Won! Scissors beats Paper"
+    } else if (playerSelection==='scissors' && computerSelection==='rock') {
+        return "You Lose! Rock beats Scissors"
+    } else if (playerSelection==='rock' && computerSelection==='paper') {
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection==='paper' && computerSelection==='scissors') {
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection==='paper' && computerSelection==='paper') {
+        return "DRAW"
+    } else if (playerSelection==='rock' && computerSelection==='rock') {
+        return "DRAW"
+    } else if (playerSelection==='Scissors' && computerSelection==='Scissors') {
+        return "DRAW"
+    } else {
+        return "Invalid input"
+    }
+}
