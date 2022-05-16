06119e59ac2fcbee1b93c1cd001fa464e4bea216
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-05-16 22:06:14 +0530

    Updating computerPlay() function

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 console.log("Rock-Paper-Scissors");
 
 function computerPlay() {
-    let compOutput = Math.floor((Math.random() * 3) + 1);
-    if (compOutput === 1) {
-        return "rock";
-    } else if (compOutput === 2) {
-        return "paper";
-    } else if (compOutput === 3) {
-        return "scissors";
-    } else {
-        console.log("None")
-    }
+    const CHOICES = ['rock', 'paper', 'scissors'];
+    const compOutput = Math.floor((Math.random() * 3) + 1);
+    return CHOICES[compOutput];
 }
 
 // console.log("Computer's choice:", computerPlay())
