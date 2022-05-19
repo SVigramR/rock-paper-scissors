9959edac0b1e7049160286c1508ee800068bfe4a
SVigramR <87016515+SVigramR@users.noreply.github.com>
2022-05-19 21:04:49 +0530

    Error fixing switch statement in playRound

--- app.js
+++ app.js
@@ -6,39 +6,30 @@ function computerPlay() {
     return choices[randomChoice];
 }
 
-// console.log("Computer's choice:", computerPlay())
-
 let playerChoice = prompt("Type your choice: ").toLowerCase();
 let computerChoice = computerPlay();
-// console.log(playRound(playerChoice, computerChoice));
+console.log(playRound(playerChoice, computerChoice));
 
 function playRound(playerSelection, computerSelection) {
     let choices = playerSelection + computerSelection;
+    let result = ''
     switch (choices) {
         case "rockscissors":
         case "paperrock":
         case "scissorspaper":
-            win();
+            result = "win";
             break;
         case "scissorsrock":
         case "rockpaper":
         case "paperscissors":
-            lose();
+            result = "lose";
             break;
         case "paperpaper":
         case "rockrock":
         case "scissorsscissors":
-            draw();
+            result = "draw";
             break;
-        default:
-            return "Invalid Input"
     }
-
+    return result ;
 }
 
-// function game(playerChoice, computerChoice){
-//     for (let i = 0; i < 5; i++) {
-//         let x[i] = playRound(playerChoice, computerChoice);
-//         console.log(x);
-//      }
-// }
